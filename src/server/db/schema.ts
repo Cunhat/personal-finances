e022d713b8a681b50198755bf8e84d990f8c693b
Tiago Cunha <tiago92cunha@gmail.com>
2024-10-30 21:43:30 +0000

    Add schemas

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,7 +3,6 @@
 
 import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-
 export const categoryGroup = sqliteTable("category_group", {
   id: integer("id", {
     mode: "number",
@@ -20,4 +19,26 @@ export const category = sqliteTable("category", {
   name: text("name").notNull(),
   icon: text("icon").notNull(),
   groupId: integer("group_id").references(() => categoryGroup.id),
+});
+
+export const account = sqliteTable("account", {
+  id: integer("id", {
+    mode: "number",
+  }).primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  accountType: text("account_type").notNull(),
+  userId: text("user_id").notNull(),
+  balance: real("balance").notNull(),
+});
+
+export const transaction = sqliteTable("transaction", {
+  id: integer("id", {
+    mode: "number",
+  }).primaryKey({ autoIncrement: true }),
+  value: real("value").notNull(),
+  created_at: text("created_at").notNull(),
+  transactionType: text("transaction_type", { enum: ["expense", "income"] as const }).notNull(),
+  categoryId: integer("category_id").references(() => category.id).notNull(),
+  userId: text("user_id").notNull(),
+  accountId: integer("account_id").references(() => account.id).notNull(),
 });
