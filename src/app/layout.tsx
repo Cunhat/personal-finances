a97daafd01ded4ef35bc8ad535d9482a107874ff
Tiago Cunha <tiago92cunha@gmail.com>
2024-11-02 16:53:21 +0000

    Add Logout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 import { AppSidebar } from "@/components/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import {
   ClerkProvider,
   RedirectToSignIn,
