1ae9a791d4ade297b2c83edca31cf36b8714e5a4
Tiago Cunha <tiago92cunha@gmail.com>
2024-11-03 12:41:27 +0000

    List categories WIP

--- src/app/(dashboard)/categories/page.tsx
+++ src/app/(dashboard)/categories/page.tsx
@@ -18,12 +18,30 @@ export default async function Page() {
   });
 
   return (
-    <div>
+    <div className="flex-1">
       <PageHeader title="Categories">
         <div className="ml-2 flex h-full w-full">
           <CreateCategory />
         </div>
       </PageHeader>
+      <div className="grid flex-1 grid-cols-2 gap-4">
+        <div className="flex flex-col gap-4">
+          {categories.map((category) => (
+            <div
+              key={category.id}
+              className="grid grid-cols-[8px_10px_1fr] items-center gap-2"
+            >
+              <div
+                className="h-2 w-2 rounded-full"
+                style={{ backgroundColor: category.color }}
+              ></div>
+              <span className="text-sm">{category.icon}</span>
+              <div className="text-base">{category.name}</div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-1 flex-col gap-4"></div>
+      </div>
     </div>
   );
 }
