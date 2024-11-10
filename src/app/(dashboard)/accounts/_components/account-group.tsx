1c7fbbddd9469378e74d80cd0d459e8ac4dd84fd
Tiago Cunha <tiago92cunha@gmail.com>
2024-11-10 09:38:28 +0000

    Add animations

--- src/app/(dashboard)/accounts/_components/account-group.tsx
+++ src/app/(dashboard)/accounts/_components/account-group.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { Account } from "@/schemas/account";
 import { ChevronDownIcon } from "lucide-react";
 import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 type AccountGroupProps = {
   groupName: string;
@@ -16,24 +18,66 @@ export default function AccountGroup({
 }: AccountGroupProps) {
   const [expanded, setExpanded] = useState(true);
 
+  const groupTotalBalance = accounts.reduce(
+    (acc, account) => acc + account.balance,
+    0,
+  );
+
   return (
-    <div>
-      <div
-        className="flex cursor-pointer items-center gap-1 rounded-md p-2 hover:bg-muted/75"
-        onClick={() => setExpanded(!expanded)}
-      >
-        <ChevronDownIcon
-          className={cn("h-4 w-4 transition-all", expanded && "rotate-180")}
-        />
-        <h1>{groupName}</h1>
-      </div>
-      {expanded && (
-        <div className="flex flex-col gap-1 p-4">
-          {accounts.map((account) => (
-            <div key={account.id}>{account.name}</div>
-          ))}
+    <AnimatePresence>
+      <div>
+        <div
+          className="flex cursor-pointer items-center gap-1 rounded-md p-2 hover:bg-muted/75"
+          onClick={() => setExpanded(!expanded)}
+        >
+          <div className="flex items-center gap-1">
+            <ChevronDownIcon
+              className={cn("h-4 w-4 transition-all", expanded && "rotate-180")}
+            />
+            <h1>{groupName}</h1>
+          </div>
+          {!expanded && (
+            <motion.p
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              className="ml-auto text-sm"
+            >
+              {groupTotalBalance} €
+            </motion.p>
+          )}
         </div>
-      )}
-    </div>
+        {expanded && (
+          <div className="flex flex-col gap-3 p-4">
+            <div className="flex flex-col gap-1">
+              {accounts.map((account, index) => (
+                <motion.div
+                  key={account.id}
+                  className="flex items-center gap-1"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.5, delay: index * 0.1 }}
+                >
+                  <div>{account.name}</div>
+                  <p className="ml-auto text-sm">{account.balance} €</p>
+                </motion.div>
+              ))}
+            </div>
+            <Separator />
+            <div className="flex items-center justify-between">
+              <motion.p
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                className="ml-auto text-sm"
+              >
+                {groupTotalBalance} €
+              </motion.p>
+            </div>
+          </div>
+        )}
+      </div>
+    </AnimatePresence>
   );
 }
