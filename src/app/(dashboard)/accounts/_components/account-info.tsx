f6c95cd57292603d24762be976aab60b2c590585
Tiago Cunha <tiago92cunha@gmail.com>
2024-11-30 21:18:33 +0000

    Add charts

--- src/app/(dashboard)/accounts/_components/account-info.tsx
+++ src/app/(dashboard)/accounts/_components/account-info.tsx
@@ -1,11 +1,17 @@
-import BalanceEvolutionTag from "@/components/balance-evolution-tag";
-import { Button } from "@/components/ui/button";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
 import { Separator } from "@/components/ui/separator";
 import { formatCurrency } from "@/lib/utils";
 import { Account } from "@/schemas/account";
-import AccountExpenses from "./account-expenses";
-import AccountActions from "./account-actions";
+import dayjs from "dayjs";
 import { Inbox } from "lucide-react";
+import { Area, AreaChart, XAxis } from "recharts";
+import AccountActions from "./account-actions";
+import AccountExpenses from "./account-expenses";
 
 type AccountInfoProps = {
   account: Account;
@@ -27,11 +33,80 @@ export default function AccountInfo({ account }: AccountInfoProps) {
     );
   }
 
+  const chartConfig = {
+    netWorth: {
+      label: "Net worth",
+      color: "hsl(var(--chart-1))",
+    },
+  } satisfies ChartConfig;
+
+  const dataOnLastYear = () => {
+    let lastYear = dayjs().subtract(1, "year");
+
+    const data: { month: string; netWorth: number }[] = [];
+
+    while (lastYear.isBefore(dayjs())) {
+      const transactions = account.transaction?.filter(
+        (transaction) =>
+          dayjs(transaction.created_at).isSame(lastYear, "month") &&
+          dayjs(transaction.created_at).isSame(lastYear, "year"),
+      );
+
+      const netWorth = transactions?.reduce((acc, transaction) => {
+        return acc + transaction.value;
+      }, 0);
+
+      data.push({
+        month: lastYear.format("MMM YYYY"),
+        netWorth: netWorth ?? 0,
+      });
+
+      lastYear = lastYear.add(1, "month");
+    }
+    return data;
+  };
+
+  const chartData = dataOnLastYear();
+
   return (
     <div className="flex h-full flex-col gap-4 overflow-hidden">
       <AccountInfoHeader account={account} />
-      <div className="flex h-[300px] w-full items-center justify-center bg-pink-200 text-black">
-        Networth chart area
+      <div className="flex h-[150px] w-full items-center justify-center text-black">
+        <ChartContainer config={chartConfig} className="h-full w-full">
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            {/* <CartesianGrid vertical={false} /> */}
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              content={
+                <ChartTooltipContent
+                  indicator="dot"
+                  labelClassName="text-white"
+                />
+              }
+            />
+            <Area
+              dataKey="netWorth"
+              type="natural"
+              fill="var(--color-netWorth)"
+              fillOpacity={0.4}
+              stroke="var(--color-netWorth)"
+              stackId="a"
+            />
+          </AreaChart>
+        </ChartContainer>
       </div>
       <Separator />
       <div className="flex flex-1 flex-col gap-2 overflow-y-auto scrollbar-none">
