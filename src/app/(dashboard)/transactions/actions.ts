6d02ae164ba9404c98c69d784123be2f1f4bf63c
Tiago Cunha <tiago92cunha@gmail.com>
2024-12-14 13:54:09 +0000

    Update account balande on create/edit/delete transation

--- src/app/(dashboard)/transactions/actions.ts
+++ src/app/(dashboard)/transactions/actions.ts
@@ -10,31 +10,56 @@ import { category, account, transaction } from "@/server/db/schema";
 import { revalidatePath, revalidateTag, unstable_cache } from "next/cache";
 import dayjs from "dayjs";
 import { z } from "zod";
-import { and, eq } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 
 export const createTransaction = authenticatedActionClient
   .schema(TransactionValidationSchema)
   .action(
     async ({
-      parsedInput: { name, amount, date, account, category, transactionType },
+      parsedInput: {
+        name,
+        amount,
+        date,
+        account: requestAccount,
+        category,
+        transactionType,
+      },
       ctx: { user },
     }) => {
       const createdAt = dayjs(date).toISOString();
 
-      await db.insert(transaction).values({
+      const newTransaction = await db.insert(transaction).values({
         //@ts-expect-error drizzle types
         name,
         value: amount,
         created_at: createdAt,
         transactionType: transactionType,
-        accountId: account,
+        accountId: requestAccount,
         categoryId: category,
         userId: user.id,
       });
 
+      if (newTransaction) {
+        await updateAccountBalance(
+          Number(requestAccount),
+          transactionType === "income" ? amount : -amount,
+        );
+      }
+
       revalidatePath("/transactions");
     },
   );
+
+const updateAccountBalance = async (accountId: number, amount: number) => {
+  await db
+    .update(account)
+    .set({
+      balance: sql`${account.balance} + ${amount}`,
+    })
+    .where(eq(account.id, accountId));
+
+  revalidatePath("/accounts");
+};
 
 export const deleteTransaction = authenticatedActionClient
   .schema(
@@ -43,9 +68,20 @@ export const deleteTransaction = authenticatedActionClient
     }),
   )
   .action(async ({ parsedInput: { id }, ctx: { user } }) => {
-    await db
+    const deletedTransaction = await db
       .delete(transaction)
-      .where(and(eq(transaction.id, id), eq(transaction.userId, user.id)));
+      .where(and(eq(transaction.id, id), eq(transaction.userId, user.id)))
+      .returning();
+
+    if (deletedTransaction.length && deletedTransaction[0]) {
+      const accountId = deletedTransaction[0].accountId;
+      const amount =
+        deletedTransaction[0].transactionType === "income"
+          ? -deletedTransaction[0].value
+          : deletedTransaction[0].value;
+
+      await updateAccountBalance(accountId, amount);
+    }
 
     revalidatePath("/transactions");
   });
@@ -53,7 +89,12 @@ export const deleteTransaction = authenticatedActionClient
 export const updateTransaction = authenticatedActionClient
   .schema(updateTransactionSchema)
   .action(async ({ parsedInput: { id, ...values }, ctx: { user } }) => {
-    await db
+    const oldTransaction = await db
+      .select()
+      .from(transaction)
+      .where(and(eq(transaction.id, id), eq(transaction.userId, user.id)));
+
+    const updatedTransaction = await db
       .update(transaction)
       .set({
         name: values.name,
@@ -63,7 +104,35 @@ export const updateTransaction = authenticatedActionClient
         categoryId: Number(values.category),
         transactionType: values.transactionType,
       })
-      .where(and(eq(transaction.id, id), eq(transaction.userId, user.id)));
+      .where(and(eq(transaction.id, id), eq(transaction.userId, user.id)))
+      .returning();
+
+    if (updatedTransaction.length && updatedTransaction[0]) {
+      const accountId = updatedTransaction[0].accountId;
+      let amount = 0;
+
+      if (
+        oldTransaction[0]?.transactionType ===
+        updatedTransaction[0]?.transactionType
+      ) {
+        const transactionTypeMultiplier =
+          oldTransaction[0]?.transactionType === "income" ? -1 : 1;
+
+        amount =
+          (oldTransaction[0].value - updatedTransaction[0].value) *
+          transactionTypeMultiplier;
+
+        await updateAccountBalance(accountId, amount);
+      } else {
+        const transactionTypeMultiplier =
+          oldTransaction[0]?.transactionType === "income" ? -1 : 1;
+        amount =
+          transactionTypeMultiplier * oldTransaction[0]!.value ??
+          0 + values.amount * transactionTypeMultiplier;
+
+        await updateAccountBalance(accountId, amount);
+      }
+    }
 
     revalidateTag("transactions");
     revalidatePath("/transactions");
