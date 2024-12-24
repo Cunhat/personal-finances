cd8296ee296c0b003b95245b6d81e152bf3ea86d
Tiago Cunha <tiago92cunha@gmail.com>
2024-12-24 14:13:54 +0000

    Fix build

--- src/app/(dashboard)/accounts/_components/account-transactions-chart.tsx
+++ src/app/(dashboard)/accounts/_components/account-transactions-chart.tsx
@@ -34,7 +34,7 @@ function AccountTransactionsChart({ data }: AccountTransactionsChartProps) {
           tickLine={false}
           tickMargin={10}
           axisLine={false}
-          tickFormatter={(value) => value.slice(0, 3)}
+          tickFormatter={(value: string) => value.slice(0, 3)}
         />
         <ChartTooltip
           cursor={false}
