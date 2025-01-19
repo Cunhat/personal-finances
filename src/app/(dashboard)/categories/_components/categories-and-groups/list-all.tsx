a39f3940079306eda5a7bacf4a57ddc7fc5afb6a
Tiago Cunha <tiago92cunha@gmail.com>
2025-01-19 14:43:21 +0000

    Adjust categories container sizes

--- src/app/(dashboard)/categories/_components/categories-and-groups/list-all.tsx
+++ src/app/(dashboard)/categories/_components/categories-and-groups/list-all.tsx
@@ -38,7 +38,7 @@ export default function ListAll({ groups, categories }: ListAllProps) {
   }
 
   return (
-    <div className="grid h-full grid-cols-[1fr_1px_1fr] gap-4 overflow-hidden">
+    <div className="grid h-full grid-cols-[1fr_1px_3fr] gap-4 overflow-hidden">
       <div className="flex flex-col gap-4 overflow-y-auto">
         <div className="flex flex-col gap-2">
           {groups.map((group) => (
